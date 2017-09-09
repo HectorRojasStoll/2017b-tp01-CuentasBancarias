a1d5ae25f136a725ecfc187eeb9bb6b5359a196a
Hector Rojas <31163116+HectorRojasStoll@users.noreply.github.com>
2017-09-09 02:14:03 -0300

    3
    AZUL declaramos variable saldo y creamos constructor de cuenta

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,11 +1,15 @@
 
 public class Cuenta {
 	
+	public int saldo;
 	
-	
+	public Cuenta(){
+		
+		saldo = 0;
+	}
 
 	public int getSaldo() {
 		// TODO Auto-generated method stub
-		return 0;
+		return saldo;
 	}
 }
